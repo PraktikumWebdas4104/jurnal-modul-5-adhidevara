5e0b2a1568ed885679ca3b29b64678a1300bd313
adhidevara <43514620+adhidevara@users.noreply.github.com>
2018-10-05 10:23:26 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td>NAMA</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>EMAIL</td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td>PASSWORD</td>
+			<td>:</td>
+			<td><input type="password" name="pass_mhs"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="daftar" value="DAFTAR"></td>
+		</tr>
+	</table>
+</form>
 
+<?php
+	if (isset($_POST['daftar'])) {
+		include 'prosesregris.php';
+	}
 
-
-</body>
-</html>
+?>
